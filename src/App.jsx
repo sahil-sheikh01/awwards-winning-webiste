c3b3e6721893966d2ab41db4647d91c0174c2729
Sahil Sheikh <you@example.com>
2026-09-04 16:25:07 +0530

    initial setup and tailwind config

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-violet-300 font-robert-regular">Welcome to Awwwards Winning Website</h1>
+    </main>
+  )
+}
+
+export default App
